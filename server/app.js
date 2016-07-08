5a7fa467346867c41d2cb6cc10f55347c2a90e74
Kenneth Korcal <kakorcal@gmail.com>
2016-07-07 23:46:33 -0700

    bug fixes. ready to scrape

--- server/app.js
+++ server/app.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const app = express();
-const server = require('http')(app);
+const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const cheerio = require('cheerio');
 const request = require('request');
 const BASE_URL = 'https://www.wikipedia.org/';
 const RANDOM_PAGE = 'Special:RandomInCategory/Featured_articles';
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(require('morgan')('dev'));
 app.use('/javascripts', express.static(`${__dirname}/../client/javascripts`));
 app.use('/stylesheets', express.static(`${__dirname}/../client/stylesheets`));
 app.use('/views', express.static(`${__dirname}/../client/views`));
-// app.use('/libs', express.static(`${__dirname}/../client/libs`));
+app.use('/libs', express.static(`${__dirname}/../client/libs`));
 
 app.get('*', (req, res)=>{
   res.sendFile('/views/layout.html', {root: './client'});
@@ -25,7 +25,7 @@ server.listen(PORT, ()=>{
 io.on('connection', socket=>{
   console.log('CLIENT HANDSHAKE');
 
-  io.on('disconnect', ()=>{
+  socket.on('disconnect', ()=>{
     console.log('CLIENT DISCONNECT');
   });
 });
